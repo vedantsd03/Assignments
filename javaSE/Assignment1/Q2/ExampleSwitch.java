51e3cd54cd9484af31994a8d354adc4a1161fc4d
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-21 16:54:09 +0530

    Create ExampleSwitch.java

---
+++ javaSE/Assignment1/Q2/ExampleSwitch.java
@@ -0,0 +1,23 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class ExampleSwitch {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter num 1 : ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter num 2 : ");
+		int num2 = sc.nextInt();
+		System.out.println("Enter  : ");
+		char a =sc.next().charAt(0);
+		switch(a) {
+		case'+':System.out.println(num1+num2);break;
+		case'-':System.out.println(num1-num2);break;
+		case'*':System.out.println(num1*num2);break;
+		case'%':System.out.println(num1%num2);break;
+		case'/':System.out.println(num1/num2);break;
+		default:System.out.println("Not a Arithmatic Operator");
+		}
+	}
+}
