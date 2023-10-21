2cfcee242afb55fa9251dff6430d23b1987f3336
vedantsd03 <148354329+vedantsd03@users.noreply.github.com>
2023-10-21 16:29:58 +0530

    Create Calculator.cs

---
+++ .net/Assignment1/Q1/Calculator.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp01
+{
+    internal class Calculator
+    {
+       public int Add(int a,int b) { 
+         return a + b; 
+       }
+        public int Product(int a,int b) { 
+            return a + b; 
+        }
+    }
+}
