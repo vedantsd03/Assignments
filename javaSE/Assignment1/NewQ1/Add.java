1839ee7adf98502cfed6c668ed0b29b8bfbbe4a6
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-21 17:10:50 +0530

    Create Add.java

---
+++ javaSE/Assignment1/NewQ1/Add.java
@@ -0,0 +1,16 @@
+1) accept two numbers and add them.
+
+import java.util.Scanner;
+class Add
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the num1 : ");
+        int num1= sc.nextInt();
+        System.out.print("Enter the num2 : ");
+        int num2= sc.nextInt();
+        System.out.println("Addition of two number is : "+(num1+num2));
+    }
+}
+
