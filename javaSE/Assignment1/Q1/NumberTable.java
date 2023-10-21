87ad631869dc28e82dd35985f12e1d1c8b473bf7
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-21 16:53:14 +0530

    Create NumberTable.java

---
+++ javaSE/Assignment1/Q1/NumberTable.java
@@ -0,0 +1,15 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class NumberTable {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number for you want to print table :");
+		int num = sc.nextInt();
+		for(int i =1;i<=10;i++) {
+			System.out.println(num+ " * " + i +" = "+ (i*num));
+		}
+	}
+}
+
