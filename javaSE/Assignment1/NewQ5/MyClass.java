6bd0e8a4fb1925aef74b7c7cfc6218a3a7a4fbb4
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-21 17:13:44 +0530

    Create MyClass.java

---
+++ javaSE/Assignment1/NewQ5/MyClass.java
@@ -0,0 +1,24 @@
+
+//5) accept two numbers and check whether first number is greater or second number is greater or they are equal.
+
+
+import java.util.Scanner;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter no 1: ");
+		int a = sc.nextInt();
+		System.out.println("Enter no 2: ");
+		int b = sc.nextInt();
+
+		if (a == b)
+			System.out.print("equal");
+		else if (a > b)
+			System.out.println("no 1 is greater");
+		else
+			System.out.println("no 2 is greater");
+	}
+}
