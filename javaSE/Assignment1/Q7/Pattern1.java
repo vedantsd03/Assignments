18b6a871db8deba6b38a80b8baf119471890e581
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-21 16:59:51 +0530

    Create Pattern1.java

---
+++ javaSE/Assignment1/Q7/Pattern1.java
@@ -0,0 +1,13 @@
+ublic class Pattern1 {
+	public static void main(String[] args) {
+		char a = 'A';
+		char b = 'a';
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				System.out.println(a +"\t"+b++);
+			}
+			a++;b='a';
+			System.out.println();
+		}
+	}
+}
