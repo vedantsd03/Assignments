8a10d7a4a744f56d68869cc0bfa3a124320127cb
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-21 16:58:06 +0530

    Create PrimeNumber.java

---
+++ javaSE/Assignment1/Q6/PrimeNumber.java
@@ -0,0 +1,15 @@
+public class PrimeNumber {
+	public static void main(String[] args) {
+		int count=0;
+		for(int i=3;i<31;i++) {
+			for(int j=2;j<i;j++) {
+				if(i%j==0)
+					count++;
+				
+			}
+			if(count==0)
+				System.out.println(i);
+			count=0;
+		}
+	}
+}
