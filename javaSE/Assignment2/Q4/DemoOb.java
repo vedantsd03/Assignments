7dc0fb8667161a9ff926c4521828289788e69e63
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-27 16:56:11 +0530

    Create DemoOb.java

---
+++ javaSE/Assignment2/Q4/DemoOb.java
@@ -0,0 +1,16 @@
+package Day2;
+class DemoOb{
+	
+	private int num ;
+	DemoOb(int num){
+		this.num = num;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
