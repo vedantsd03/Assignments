920173c55d7f242915cc7db6b905460b0a3285dd
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-27 16:59:53 +0530

    Create C.java

---
+++ javaSE/Assignment2/Q5/C.java
@@ -0,0 +1,17 @@
+
+class C{
+	private static int nums=30 ;
+	private int num;
+	
+	C(int num){
+		this.num = num;
+	}
+
+	public static int getNums() {
+		return nums;
+	}
+
+	public int getNum() {
+		return num;
+	}
+}
