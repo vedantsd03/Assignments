5bc1df27d194fa81ced576f18c9e618dd0b1194b
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-27 16:52:42 +0530

    Create Emp.java

---
+++ javaSE/Assignment2/Q3/Emp.java
@@ -0,0 +1,16 @@
+
+/*
+
+3) Define a class "Emp" with private static member "int cnt".
+How will u make sure that outsiders can read the value of cnt ?
+*/
+
+
+package Day2;
+class Emp{
+	public static void main(String [] args) {
+		System.out.println("The value of cnt : "+EmpInfo.getCnt());
+	}
+}
+
+
