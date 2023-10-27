7c1b64944874bda8c31c81983eeb3dc9f9c31a10
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-27 16:58:54 +0530

    Create A.java

---
+++ javaSE/Assignment2/Q5/A.java
@@ -0,0 +1,17 @@
+package Day2;
+class A{
+	private static int nums=30 ;
+	private int num;
+	
+	A(int num){
+		this.num = num;
+	}
+
+	public static int getNums() {
+		return nums;
+	}
+
+	public int getNum() {
+		return num;
+	}
+}
