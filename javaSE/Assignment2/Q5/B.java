d0766c1f13045bdb9d3111eacd63368c903287b0
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-27 16:59:23 +0530

    Create B.java

---
+++ javaSE/Assignment2/Q5/B.java
@@ -0,0 +1,37 @@
+
+class B{
+	private static int nums=30 ;
+	private int num;
+	
+	B(int num){
+		this.num = num;
+	}
+
+	public static int getNums() {
+		return nums;
+	}
+
+	public int getNum() {
+		return num;
+	}
+	
+}
+
+class C{
+	private static int nums=30 ;
+	private int num;
+	
+	C(int num){
+		this.num = num;
+	}
+
+	public static int getNums() {
+		return nums;
+	}
+
+	public int getNum() {
+		return num;
+	}
+	
+	
+}
