3dff0d5adb5b56c5b73b89d1f53088ece03a665c
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-28 16:56:42 +0530

    Create Account.cs

---
+++ .net/Assignment6/Q3/Account.cs
@@ -0,0 +1,41 @@
+namespace ConsoleApp2
+{
+    internal class Account
+    {
+        int id;
+        string name;
+        double balance;
+        static int sid;
+
+        static Account()
+        {
+            Console.WriteLine("Welcome to State Bank of India \n");
+        }
+        public Account( string name, double balance)
+        {
+            this.id = ++sid;
+            this.name = name;
+            this.balance = balance;
+        }
+        public int ID
+        {
+            get { return id; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name=value; }
+        }
+        public double Balance
+        {
+            get { return balance; }
+            protected set { balance = value; }
+        }
+        public void Deposit(double d)
+        {
+            Console.WriteLine("***Moneey deposited sucessfully***");
+            Balance += d;
+        }
+       
+    }
+}
