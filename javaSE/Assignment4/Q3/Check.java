a9389014774912bc5a281314a604a5d1d4292e2b
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:09:25 +0530

    Create Check.java

---
+++ javaSE/Assignment4/Q3/Check.java
@@ -0,0 +1,19 @@
+
+package day_4;
+
+class Check {
+	
+	int a = 10;
+	private int b = 12;
+	protected int c =14;
+	public int d = 16;
+	public void disp() {
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println("Hello form the Check class dispPublic()");
+	}
+	
+}
