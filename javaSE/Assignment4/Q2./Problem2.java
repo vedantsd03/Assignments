60ca8e324271e8a2b1f33bbde9ad2b4209c25350
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:07:56 +0530

    Create Problem2.java

---
+++ javaSE/Assignment4/Q2./Problem2.java
@@ -0,0 +1,17 @@
+
+package day_4;
+
+class Problem2{
+	static int a ;
+	int b ;
+	
+	public Problem2(int b) {
+		this.b = b;
+	}
+	public static void dispA() {
+		System.out.println(a);
+	}
+	public  void dispB() {
+		System.out.println(b);
+	}
+}
