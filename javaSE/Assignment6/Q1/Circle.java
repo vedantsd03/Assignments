1ef9437e1d49260458d306b4611cde2867c5a11e
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:58:24 +0530

    Create Circle.java

---
+++ javaSE/Assignment6/Q1/Circle.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package devpack;
+
+/**
+ * 
+ */
+public class Circle implements Shape{
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Draw Circle");
+	}
+	
+}
