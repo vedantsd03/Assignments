f376688291b8f73ebe0e897f31a089f210fd3161
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:10:24 +0530

    Create Problem4Demo.java

---
+++ javaSE/Assignment4/Q4/Problem4Demo.java
@@ -0,0 +1,8 @@
+//4)	Show the example of single level inheritance with constructor invocation.
+
+package day_4;
+public class Problem4Demo {
+	public static void main(String[] args) {
+		Child c = new Child();
+	}
+}
