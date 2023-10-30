c79b7ef7266c9ab7a65652a2e5828ac5bec80853
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:31:38 +0530

    Create Demo.java

---
+++ javaSE/Assignment5/Q1/Demo.java
@@ -0,0 +1,13 @@
+/*1) Create a multi-level inheritance , instantiate the child class and observe constructor invocation.
+Also show, if needed how will u invoke parent class constructor from child class ?
+*/
+package Problem1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		 new Sub2();
+		
+	}
+
+}
