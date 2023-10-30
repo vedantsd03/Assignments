f549ca39b5a525b05e8b3166ac043bdd80b38681
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:12:42 +0530

    Create Problem5Demo.java

---
+++ javaSE/Assignment4/Q5/Problem5Demo.java
@@ -0,0 +1,9 @@
+//5)	Show the example of multi-level inheritance with constructor invocation.
+
+package day_4;
+public class Problem5Demo {
+	public static void main(String[] args) {
+			Sub2 s2 = new Sub2();
+			
+	}
+}
