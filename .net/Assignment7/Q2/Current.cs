3bda4527f47e19bc2a65f2235679441dc2d7576e
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 12:47:43 +0530

    Create Current.cs

---
+++ .net/Assignment7/Q2/Current.cs
@@ -0,0 +1,12 @@
+
+namespace AccountDemo
+{
+    internal class Current : Account
+    {
+        public Current(string name) : base(name) { }
+        public override void withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
