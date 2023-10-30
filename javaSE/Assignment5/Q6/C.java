6d652743f73306fb3f9369babbdcd26b0f71e542
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:46:56 +0530

    Create C.java

---
+++ javaSE/Assignment5/Q6/C.java
@@ -0,0 +1,16 @@
+
+package Problem1;
+
+class C implements B{
+
+	@Override
+	public void disp11() {
+		System.out.println("From disp11 method");		
+	}
+
+	@Override
+	public void disp22() {
+		System.out.println("From disp22 method");		
+	}
+	
+}
