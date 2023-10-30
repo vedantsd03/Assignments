edcdae2b5a7c4e025a7630aa4ccd23a4d84a1407
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:33:56 +0530

    Create Demo1.java

---
+++ javaSE/Assignment5/Q2/Demo1.java
@@ -0,0 +1,14 @@
+/*
+2) create a class "Vehicle", define a method "public void start()" in it.
+From this class derive a class FourWheeler. How will u override "start()" method of parent class ?
+*/
+package Problem2;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		Vehicle v =  new FourWheeler();
+		v.start();
+
+	}
+
+}
