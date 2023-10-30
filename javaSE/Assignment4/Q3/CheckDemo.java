e799a05971e220d619f0753bbff46067042e43d8
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:09:01 +0530

    Create CheckDemo.java

---
+++ javaSE/Assignment4/Q3/CheckDemo.java
@@ -0,0 +1,10 @@
+
+public class CheckDemo {
+public static void main(String[] args) {
+	Check c1 = new Check();
+	System.out.println(c1.a);
+	//System.out.println(c1.b); //cte as its access outside the class
+	System.out.println(c1.d);
+	System.out.println(c1.c);
+}
+}
