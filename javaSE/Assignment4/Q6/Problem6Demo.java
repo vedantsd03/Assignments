24a03126e3e8b1979027e859fada13102ce587f8
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:16:49 +0530

    Create Problem6Demo.java

---
+++ javaSE/Assignment4/Q6/Problem6Demo.java
@@ -0,0 +1,13 @@
+//6)	Show the example of hierarchical inheritance with constructor invocation.
+
+package day_4_1;
+
+public class Problem6Demo {
+
+	public static void main(String[] args) {
+		SubClass1 s1 = new SubClass1();
+		SubClass2 s2 = new SubClass2();
+		SubClass3 s3 = new SubClass3();
+
+	}
+}
