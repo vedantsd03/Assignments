cc1ac4070705c4010f0d41eed58d008692afb423
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:36:06 +0530

    Create Engineering.java

---
+++ javaSE/Assignment5/Q3/Engineering.java
@@ -0,0 +1,8 @@
+//3) Go for Hierarchical inheritance, create instances of child class and observe constructor invocation.
+package Problem1;
+
+class Engineering{
+	public void assignCourse() {
+		System.out.println("course Assign");
+	}
+}
