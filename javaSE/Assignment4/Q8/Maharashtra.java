19f2d94db1052305934984203f51c7bf4b064dad
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-10-30 19:20:38 +0530

    Create Maharashtra.java

---
+++ javaSE/Assignment4/Q8/Maharashtra.java
@@ -0,0 +1,9 @@
+package day_4_1;
+class Maharashtra extends India {
+
+	public void area(int area) { // overloading
+
+		System.out.print("Area of Maharashtra is...");
+		System.out.println(area);
+	}
+}
