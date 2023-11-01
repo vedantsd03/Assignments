5e7abe804a4753f9f56b47a74685ce2e98bbe263
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 18:44:54 +0530

    Create MyException.cs

---
+++ .net/Assignment9/Q1/MyException.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionProject
+{
+    
+    internal class MyException : ApplicationException
+    {
+        public MyException(string name):base(name) {
+          
+        }
+    }
+}
