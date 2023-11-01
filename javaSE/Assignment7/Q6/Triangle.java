3a1f07df0bb9abc6bc8bb4e974c408805dd6f759
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 17:05:06 +0530

    Create Triangle.java

---
+++ javaSE/Assignment7/Q6/Triangle.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package devpack;
+
+/**
+ * @author Vedant
+ */
+public class Triangle implements Shape {
+	
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Draw triangle");
+	}
+}
