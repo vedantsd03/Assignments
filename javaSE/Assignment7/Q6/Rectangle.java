609f19b59a233c6d696f66b41be5f46dc468dda8
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 17:04:47 +0530

    Create Rectangle.java

---
+++ javaSE/Assignment7/Q6/Rectangle.java
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ */
+package devpack;
+
+/**
+ * 
+ */
+public class Rectangle implements Shape{
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Draw Rectangle");
+	}
+	
+}
