7088b37ec112652523d0c79825b275314c271078
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 18:45:21 +0530

    Create Program.cs

---
+++ .net/Assignment9/Q1/Program.cs
@@ -0,0 +1,11 @@
+namespace ExceptionProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AgeCheker ageCheker = new AgeCheker();
+            ageCheker.CheckAge(190);
+        }
+    }
+}
