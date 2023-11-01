9d169c3527699a21f879b3c14c44d3747bf6e2ec
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 20:03:58 +0530

    Create Linq_1.cs

---
+++ .net/Assignment10/LINQ_Lamda/Q1/Linq_1.cs
@@ -0,0 +1,29 @@
+//Q1.display all number<=5
+
+namespace ConsoleApplication3
+{
+    internal class Linq_1
+    {
+        static void Main(string[] args)
+        {
+            QueryOverInts();
+        }
+        static void QueryOverInts()
+        {
+            int[] a = { 5, 8, 9, 2, 1 };
+
+            var subset = from i in a where i <=5 select i;
+
+            // LINQ statement evaluated here!
+            foreach (var i in subset)
+                Console.WriteLine("{0} < 10", i);
+            Console.WriteLine();
+
+            //using Lamda
+            var subset2 = a.Where(r => r<=5).Select(c => c);
+            foreach (var i in subset2)
+                Console.WriteLine(i);
+
+        }
+    }
+}
