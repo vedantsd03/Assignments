69e9f14779a658c9d9b4c851ccf72d97217f7a96
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 17:08:40 +0530

    Create IndianPizzaIngredientFactory.java

---
+++ javaSE/Assignment7/Q7/IndianPizzaIngredientFactory.java
@@ -0,0 +1,24 @@
+
+class IndianPizzaIngredientFactory implements PizzaIngredientFactory
+{
+	public Dough createDough()
+	{
+		return new Dough();
+	}
+	public Sauce createSauce()
+	{
+		return new Sauce();
+	}
+	public Cheese createCheese()
+	{
+		return new Cheese();
+	}
+	public Pepperoni createPepperoni()
+	{
+		return new Pepperoni();
+	}
+	public Clams createClams()
+	{
+		return new Clams();
+	}
+}
