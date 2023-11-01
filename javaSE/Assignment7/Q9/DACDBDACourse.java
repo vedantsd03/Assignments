b64ef8686a6c0b6807bbcb92b6879fa9bcb0e056
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 17:16:10 +0530

    Create DACDBDACourse.java

---
+++ javaSE/Assignment7/Q9/DACDBDACourse.java
@@ -0,0 +1,16 @@
+
+package Problem8;
+
+public class DACDBDACourse implements Course{
+	
+	public void start() {
+		System.out.println("DACDBDACourse Course is Started");
+	}
+	public void Stop() {
+		System.out.println("DACDBDACourse Course Stoped");
+	}
+	
+	public void orientation() {
+		System.out.println("Orientation is Started for DACDBDACourse");
+	}
+}
