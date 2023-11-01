130710b677cadc3c47a5a253a92a9cbb2b90ace9
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 17:01:46 +0530

    Create UIComponentCreator.java

---
+++ javaSE/Assignment7/Q4/UIComponentCreator.java
@@ -0,0 +1,17 @@
+
+public abstract class UIComponentCreator {
+	
+	public void show(String type)
+	{
+		UIComponent comp = createUIComponent(type);
+		add(comp);
+	}
+	
+	public void add(UIComponent component)
+	{
+		System.out.println("Added the component\t"+component.getClass().getName());
+	}
+	
+	public abstract UIComponent createUIComponent(String type);
+
+}
