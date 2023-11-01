791d52201c52367f321bc6c416112db44d841c13
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 16:54:39 +0530

    Create Demo1.java

---
+++ javaSE/Assignment7/Q2/Demo1.java
@@ -0,0 +1,40 @@
+/*2)
+abstract class Beverages {
+
+	// Defined
+	public void addMilk() {
+
+	}
+
+	// abstract
+	abstract void addIngredients();
+}
+
+class Coffee extends Beverages
+{
+	void addIngredients()
+	{
+		// ingredients for Coffee
+	}
+	// addMilk inherited
+}
+class Tea extends Beverages
+{
+	void addIngredients()
+	{
+		// ingredients for Tea
+	}
+	// addMilk inherited
+}
+class FruitJuice extends Beverages
+{
+	void addIngredients()
+	{
+		// ingredients for FruitJuice
+	}
+	// addMilk inherited
+}
+
+create Array of Beverages class and store all the child classes objects.
+now traverse the array and invoke all the functions.
+*/
