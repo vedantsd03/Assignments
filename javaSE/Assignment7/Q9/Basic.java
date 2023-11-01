a6360153a724bb63b907fa9220c4caad856313d8
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 17:15:43 +0530

    Create Basic.java

---
+++ javaSE/Assignment7/Q9/Basic.java
@@ -0,0 +1,14 @@
+
+package Problem8;
+
+public class Basic implements Course{
+	
+	public void start() {
+		System.out.println("Basic Course is Started");
+	}
+	public void Stop() {
+		System.out.println("Basic Course Stoped");
+	}
+}
+
+
