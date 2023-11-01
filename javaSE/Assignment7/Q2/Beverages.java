0d5ffa062a372f0dd6f26061685155dd0b8d4253
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 16:56:05 +0530

    Create Beverages.java

---
+++ javaSE/Assignment7/Q2/Beverages.java
@@ -0,0 +1,12 @@
+package Problem1;
+
+public abstract class Beverages {
+
+	// Defined
+	public void addMilk() {
+		System.out.println("Milk is add ed in the Drink");
+	}
+
+	// abstract
+	abstract void addIngredients();
+}
