ee5d1a8fd91745bf01fc4230491c4b2087666a7c
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 16:49:15 +0530

    Create Gun.java

---
+++ javaSE/Assignment6/Q5/Gun.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package weaponpack;
+
+/**
+ * @author Vedant
+ *
+ */
+public class Gun extends Weapon {
+	void attack() {
+		System.out.println("using gun to attack");
+	}
+	
+	void fillBullets() {
+		System.out.println("filling bullets");
+	}
+}
