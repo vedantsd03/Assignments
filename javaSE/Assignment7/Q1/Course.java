a6a8529a1a88aebb227ba1d8538c84674f26fca3
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 16:53:28 +0530

    Create Course.java

---
+++ javaSE/Assignment7/Q1/Course.java
@@ -0,0 +1,12 @@
+
+package Problem1;
+
+public abstract class Course {
+	public void start()
+	{
+		System.out.println("The Course is started Welcome Students");
+	}
+	public abstract double getFee();
+	public abstract void evaluate();
+}
+
