db33f8c487310e8d49323e5e25acc74fac1fd87b
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 16:48:30 +0530

    Create WeaponDemo.java

---
+++ javaSE/Assignment6/Q5/WeaponDemo.java
@@ -0,0 +1,22 @@
+
+/**
+ * 
+ */
+package weaponpack;
+
+/**
+ * @author Vedant
+ *
+ */
+public class WeaponDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Soldier s1=new Soldier();
+		s1.fight(new Gun());
+	}
+}	
+
