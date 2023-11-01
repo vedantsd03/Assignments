d5f9ab15db06eb73da9235a8a3b67c7c7f90109c
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 17:03:25 +0530

    Create CommandLine.java

---
+++ javaSE/Assignment7/Q5/CommandLine.java
@@ -0,0 +1,14 @@
+//5)accept 3 numbers through command line arguments and display their sum.
+
+class CommandLine
+{
+	public static void main(String [] args)
+	{
+		int sum=0;
+        	for(int i =0;i<args.length;i++)
+		{
+			sum+=Integer.parseInt(args[i]);
+		}
+		System.out.println(sum);
+	}
+}
