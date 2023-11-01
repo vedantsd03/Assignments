8d9cde06a6d4453785b6303eae88cc3aa8e4ce71
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 16:51:02 +0530

    Update WeaponDemo.java

--- javaSE/Assignment6/Q5/WeaponDemo.java
+++ javaSE/Assignment6/Q5/WeaponDemo.java
@@ -1,4 +1,53 @@
+/*
+5) Code this:
+create a new project
+	create a package "weaponpack"
+		inside this package
 
+	public class Weapon
+	{
+		public void attack()
+		{
+		}
+	}
+	public class Sword extends Weapon
+	{
+		override attack
+	}
+	public class Bomb extends Weapon
+	{
+		override attack
+	}
+	public class Gun extends Weapon
+	{
+		override attack
+			+
+		public void fillBullets()
+		{
+			S.o.p("filling bullets");
+		}
+	}
+
+	public class Soldier
+	{
+		// define "fight" method in such a way that given any specific weapon, it should be able to attack
+		public void fight(Weapon ref)
+		{
+			ref.attack();
+			// make sure you invoke "fillBullets" also along with "attack" and that shouldn't give "ClassCastException"
+		}
+
+	}
+	public class WeaponDemo
+	{
+		p.s.v.main()
+		{
+			//invoke "fight" method of Soldier class
+			Soldier s1=new Soldier();
+			s1.fight(new Gun());
+		}
+	}
+*/
 /**
  * 
  */
