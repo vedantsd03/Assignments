826ca19be923bf1eaeddeb113f5935ed1eeec896
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 20:07:30 +0530

    Create Linq_3.cs

---
+++ .net/Assignment10/LINQ_Lamda/Q3/Linq_3.cs
@@ -0,0 +1,29 @@
+//display all odd number
+
+namespace ConsoleApplication3
+{
+    internal class Linq_3
+    {
+        static void Main(string[] args)
+        {
+            QueryOverInts();
+        }
+        static void QueryOverInts()
+        {
+            int[] a = { 5, 8, 9, 2, 1 };
+
+            var subset = from i in a where i%2!=0 select i;
+
+            // LINQ statement evaluated here!
+            foreach (var i in subset)
+                Console.WriteLine(i);
+            Console.WriteLine();
+
+            //using Lamda
+            var subset2 = a.Where(r =>r%2!=0).Select(c => c);
+            foreach (var i in subset2)
+                Console.WriteLine(i);
+
+        }
+    }
+}
