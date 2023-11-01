44c7049d0662ed7a46e0cf57912f148a40a42f84
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-01 17:15:54 +0530

    Create MSCit.java

---
+++ javaSE/Assignment7/Q9/MSCit.java
@@ -0,0 +1,10 @@
+package Problem8;
+
+public class MSCit implements Course{
+	public void start() {
+		System.out.println("MSCit Course is Started");
+	}
+	public void Stop() {
+		System.out.println("MSCit Course Stoped");
+	}
+}
