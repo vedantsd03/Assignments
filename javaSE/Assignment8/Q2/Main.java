ea9e7eff0044fe1c73b49fd5712dd1d38ac184a8
Vedant S Dhadange <148354329+vedantsd03@users.noreply.github.com>
2023-11-05 12:47:16 +0530

    Create Main.java

---
+++ javaSE/Assignment8/Q2/Main.java
@@ -0,0 +1,17 @@
+/*
+2) on the developer side create an exception:
+	NumberNotDivisibleBySevenException
+	as a checked exception
+
+create necessary jar and documentation.
+
+on client side
+	public class MyMath class with 
+		public void disp(int num)
+this disp() method will check if the number passed is not divisible by 7 , it will raise "NumberNotDivisibleBySevenException" or else it will simply display the number passed.
+	[ this method shouldn't handle the exception]
+
+	public class Demo
+		main function 
+		invoke "disp()" of "MyMath" class.
+*/
